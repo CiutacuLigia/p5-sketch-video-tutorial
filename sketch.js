c8cfe8ab060afc52453931d81573eafe55cb7d52
mathematician8 <96425330+mathematician8@users.noreply.github.com>
2024-04-10 11:01:18 +0300

    Uploading p5.js sketch files from my desktop
    It's a smiley face.

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(124, 220, 130);
+  circle(200, 200, 100);
+  stroke('blue');
+  strokeWeight(10);
+  point(180, 175);
+  point(215, 175);
+  arc(200, 200, 60, 60, TWO_PI, PI);
+}
